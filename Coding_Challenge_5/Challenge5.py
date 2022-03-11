4bad965037f1cae92d9d51e1445cb98da668c366
Maegan Shanaghan <98616412+mshanaghan@users.noreply.github.com>
2022-03-11 13:02:37 -0500

    Add files via upload

--- Coding_Challenge_5/Challenge5.py
+++ Coding_Challenge_5/Challenge5.py
@@ -1,4 +1,22 @@
 # Coding Challenge 5
+# For this coding challenge, I want you to practice the heatmap generation that we went through in class,
+# but this time obtain your own input data, and I want you to generate heatmaps for TWO species.
+# You can obtain species data from a vast array of different sources, for example:
+# obis - Note: You should delete many columns (keep species name, lat/lon) as OBIS adds some really long strings
+# that don't fit the Shapefile specification.
+# GBIF
+# Maybe something on RI GIS
+# Or just Google species distribution data
+# My requirements are:
+# The two input species data must be in a SINGLE CSV file, you must process the input data to separate out the species
+# (Hint: You can a slightly edited version of our CSV code from a previous session to do this),
+# I recommend downloading the species data from the same source so the columns match.
+# Only a single line of code needs to be altered (workspace environment) to ensure code runs on my computer,
+# and you provide the species data along with your Python code.
+# The heatmaps are set to the right size and extent for your species input data, i.e. appropriate fishnet cellSize.
+# You leave no trace of execution, except the resulting heatmap files.
+# You provide print statements that explain what the code is doing, e.g. Fishnet file generated.
+
 # "combined_csv.csv" contains location data for 2 fish species
 import arcpy
 arcpy.env.overwriteOutput = True
@@ -131,4 +149,6 @@ arcpy.SpatialJoin_analysis(target_features, join_features, out_feature_class,
                            search_radius, distance_field_name)
 if arcpy.Exists(out_feature_class):
     print("Created Heatmap 2 successfully!")
+
+
 
