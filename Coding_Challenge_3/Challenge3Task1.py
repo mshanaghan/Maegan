dd62c83551fb35a0d06b374ed457f3137fada0d1
Andy Davies <andy.marinebio@googlemail.com>
2022-03-29 14:22:53 -0400

    AD Feedback - Made some suggested changes

--- Coding_Challenge_3/Challenge3Task1.py
+++ Coding_Challenge_3/Challenge3Task1.py
@@ -9,14 +9,16 @@
 # |   └── post
 # |       └── posted
 # └── site
-import os
+import os, shutil
 # # Using os.system or os.mkdirs replicate this simple directory tree.
-os.makedirs("E:/draft_code/pending")
-os.makedirs("E:/draft_code/complete")
-os.makedirs("E:/includes")
-os.makedirs("E:/layouts/default")
-os.makedirs("E:/layouts/post/posted")
-os.makedirs("E:/site")
+root = "C:\Test"
+
+os.makedirs(os.path.join(root, "draft_code/pending"))
+os.makedirs(os.path.join(root, "draft_code/complete"))
+os.makedirs(os.path.join(root, "includes"))
+os.makedirs(os.path.join(root, "layouts/default"))
+os.makedirs(os.path.join(root, "layouts/post/posted"))
+os.makedirs(os.path.join(root, "site"))
 # # Yay it worked
 
 # # Delete the directory tree without deleting your entire hard drive.
@@ -26,3 +28,6 @@ os.rmdir("E:/includes")
 os.rmdir("E:/layouts/default")
 os.rmdir("E:/layouts/post/posted")
 os.rmdir("E:/site")
+
+#Alteranively - be careful
+#shutil.rmtree(root)
--- Coding_Challenge_3/Challenge3Task2.bat
+++ Coding_Challenge_3/Challenge3Task2.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:\ShanaghanNRS528\Challenge3Task2.py" Fish Dog Cat
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "Challenge3Task2.py" Fish Dog Cat
 pause
