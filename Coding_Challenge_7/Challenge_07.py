a32f7253be426747fb6e98b25c2ba5b5917c54de
Andy Davies <andy.marinebio@googlemail.com>
2022-04-18 14:18:03 -0400

    AD Feedback - Made some suggested changes

--- Coding_Challenge_7/Challenge_07.py
+++ Coding_Challenge_7/Challenge_07.py
@@ -8,24 +8,36 @@ import os
 # "combined_csv.csv" contains location data for 2 fish species
 import arcpy
 import csv
+import glob
+
+####
+# User changable
+####
+
 arcpy.env.overwriteOutput = True
 # Set workspace
-arcpy.env.workspace = "E:\ShanaghanPythonGIS\Class_Files\Class07"
-input_directory = "E:\ShanaghanPythonGIS\Class_Files\Class07"
-# Create temporary file folder using os.path.join
+input_directory = "C:\Data\Students_2022\Shanaghan\Coding_Challenge_7"
+arcpy.env.workspace = input_directory
+
+# Create temporary and output file folder using os.path.join
 if not os.path.exists(os.path.join(input_directory, "temporary_files")):
     os.mkdir(os.path.join(input_directory, "temporary_files"))
-temporary_files = "E:\ShanaghanPythonGIS\Class_Files\Class07\temporary_files"
+if not os.path.exists(os.path.join(input_directory, "output_files")):
+    os.mkdir(os.path.join(input_directory, "output_files"))
+
+species_file_list = glob.glob("*.csv") # I am assuming only the one csv file here.
+
+temporary_files = os.path.join(input_directory, "temporary_files")
 # # Set spatial reference of output
 spRef = arcpy.SpatialReference(4326)
 # Convert .csv to a shapefile which contains both species
 
-in_Table = r"combined_csv.csv"
+in_Table = os.path.join(input_directory, species_file_list[0])
 x_coords = "decimallongitude"
 y_coords = "decimallatitude"
 z_coords = ""
 out_Layer = "fish_locations"
-saved_Layer = r"fish_locations_output.shp"
+saved_Layer = os.path.join(input_directory, "temporary_files", r"fish_locations_output.shp")
 lyr = arcpy.MakeXYEventLayer_management(in_Table, x_coords, y_coords, out_Layer, spRef, z_coords)
 arcpy.CopyFeatures_management(lyr, saved_Layer)
 if arcpy.Exists(saved_Layer):
@@ -33,7 +45,7 @@ if arcpy.Exists(saved_Layer):
 
 # Use csv to extract a list of unique species. Call it species_list
 species_list = []
-with open("combined_csv.csv") as species_csv:
+with open(os.path.join(input_directory, species_file_list[0])) as species_csv:
     next(species_csv) #skip first line
     for row in csv.reader(species_csv):
         if row[2] not in species_list:
@@ -45,11 +57,11 @@ print(len(species_list))
 for species in species_list:
      query = "scientific = " + "'" + str(species) + "'"
      print(query)
-     selection = arcpy.management.SelectLayerByAttribute("fish_locations_output.shp", "NEW_SELECTION", query)
-     arcpy.CopyFeatures_management(selection, "Species" + str(species) + ".shp")
-     file = open(os.path.join(input_directory, "temporary_files"))
+     selection = arcpy.management.SelectLayerByAttribute(saved_Layer, "NEW_SELECTION", query)
+     arcpy.CopyFeatures_management(selection, os.path.join(input_directory, "temporary_files", "Species" + str(species) + ".shp"))
+
 # # # Extract the Extent, i.e. XMin, XMax, YMin, YMax
-     desc = arcpy.Describe("Species" + str(species) + ".shp")
+     desc = arcpy.Describe(os.path.join(input_directory, "temporary_files", "Species" + str(species) + ".shp"))
      XMin = desc.extent.XMin
      XMax = desc.extent.XMax
      YMin = desc.extent.YMin
@@ -57,7 +69,7 @@ for species in species_list:
 # # # Generate fishnet using extent details
 
      spRef = arcpy.SpatialReference(4326)
-     outFeatureClass = "Species_fishnet_" + str(species) + ".shp"
+     outFeatureClass = os.path.join(input_directory, "temporary_files", "Species_fishnet_" + str(species) + ".shp")
      originCoordinate = str(XMin) + " " + str(YMin)
      yAxisCoordinate = str(XMin) + " " + str(YMin + 1)
      cellSizeWidth = "0.5"
@@ -76,9 +88,9 @@ for species in species_list:
 
 # Use spatial join to create heat maps
 
-     target_features = "Species_fishnet_" + str(species) + ".shp"
-     join_features = "Species" + str(species) + ".shp"
-     out_feature_class = "Species_heatmap_" + str(species) + ".shp"
+     target_features = os.path.join(input_directory, "temporary_files", "Species_fishnet_" + str(species) + ".shp")
+     join_features = os.path.join(input_directory, "temporary_files", "Species" + str(species) + ".shp")
+     out_feature_class = os.path.join(input_directory, "output_files", "Species_heatmap_" + str(species) + ".shp")
      join_operation = "JOIN_ONE_TO_ONE"
      join_type = "KEEP_ALL"
      field_mapping = ""
@@ -89,5 +101,5 @@ for species in species_list:
                                 join_operation, join_type, field_mapping, match_option,
                                 search_radius, distance_field_name)
      if arcpy.Exists(out_feature_class):
-         print("Created Heatmap file successfully for" + str(species))
+        print("Created Heatmap file successfully for" + str(species))
 
